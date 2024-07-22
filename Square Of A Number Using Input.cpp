d0a4e34324e2112b47b38665868a01be6d8572b8
SHIVAM GUPTA <shivamgupta95750@gmail.com>
2024-07-22 13:53:21 +0530

    Add files via upload

---
+++ Square Of A Number Using Input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+ using namespace std;
+ int main(){
+     int x;
+     cout<<"Enter the number:";
+     cin>>x;
+     cout<<"Square of this number is:";
+     cout<<x*x;
+ }
---
+++ Volume Of A Sphere.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float r=5;
+  float v=4*3.1415*r*r*r/3;  //[ volume of a sphere is (4/3 pir r cube) ]
+  cout<<v;
+}
