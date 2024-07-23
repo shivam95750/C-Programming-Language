4e743d93789e3c13e5f76bfb2aaef5527e658e31
SHIVAM GUPTA <shivamgupta95750@gmail.com>
2024-07-23 18:00:37 +0530

    Add files via upload

---
+++ Product of numbers using inputs.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    int z;
+    cout<<"Enter Your first Number :";
+    cin>>x;
+    cout<<"Enter Your second Number :";
+    cin>>y;
+    cout<<"Enter Your Third Number :";
+    cin>>z;
+    cout<<"Product Is :" <<x*y*z;
+}
---
+++ Sum Of Two variable By Inputs.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    int y;
+        cout<<"Enter Your First Number :";
+        cin>>x;
+        cout<<"Enter Your Second First Number :";
+        cin>>y;
+        cout<<"Sum Is :" <<x+y;
+}
